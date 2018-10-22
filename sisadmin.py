c0b9c726d81175323ce1fd193c94533069d5fd6d
AlexanderUsatov <41695664+AlexanderUsatov@users.noreply.github.com>
2018-10-22 16:39:43 +0300

    Create sisadmin.py

---
+++ sisadmin.py
@@ -0,0 +1,30 @@
+s = input().strip().split(' ')
+
+n = int(s[0])
+m = int(s[1])
+v = int(s[2]) - 1
+
+if m > (n - 1) * (n - 2) / 2 + 1 or m < n - 1:
+    print(-1)
+    exit(0)
+
+if v == 0:
+    ignore = n - 1
+else:
+    ignore = 0
+
+for i in range(0, n):
+    if i != v:
+        print(str(i + 1) + ' ' + str(v + 1))
+m -= n - 1
+
+for i in range(0, n):
+    if i == v or i == ignore:
+        continue
+    for j in range(i + 1, n):
+        if j == v or j == ignore:
+            continue
+        if m == 0:
+            exit(0)
+        print(str(i + 1) + ' ' + str(j + 1))
+        m -= 1
