0a034073ec5a41a844ae8b2b7d34a035bc765494
AlexanderUsatov <41695664+AlexanderUsatov@users.noreply.github.com>
2018-10-22 17:39:18 +0300

    Update two_buttons.py

--- two_buttons.py
+++ two_buttons.py
@@ -4,24 +4,30 @@ s = input().strip().split(' ')
 n = int(s[0])
 m = int(s[1])
 
-g = [[] for i in range(20_000 + 1)]
+if m <= n:
+    print(n - m)
+    exit(0)
 
-for i in range(1, 10_001):
+g = [[] for i in range(2 * m - 1)]
+
+for i in range(1, m):
     if i < m:
         g[i].append(i * 2)
 
-for i in range(2, 20_001):
+for i in range(2, 2 * m - 1):
     g[i].append(i - 1)
 
-out = [0] * (20_000 + 1)
+out = [0] * (2 * m - 1)
 q = Queue()
 q.put((n, 0))
+out[n] = 1
 
 while not q.empty():
     curr = q.get()
     for i in g[curr[0]]:
-        if i == m:
-            print(curr[1] + 1)
-            exit(0)
-        q.put((i, curr[1] + 1))
-        out[curr[0]] = 1
+        if not out[i]:
+            if i == m:
+                print(curr[1] + 1)
+                exit(0)
+            q.put((i, curr[1] + 1))
+            out[i] = 1
