eaf24d8f2c6ff12d9a77547e212da49d171ccc2f
AlexanderUsatov <41695664+AlexanderUsatov@users.noreply.github.com>
2018-10-25 19:17:27 +0300

    Update KefaAndPark.java

--- KefaAndPark.java
+++ KefaAndPark.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class Main {
+public class KefaAndPark {
 
     static int[] cats;
     static int m;
